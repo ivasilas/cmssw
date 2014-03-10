112295e0794930e1706cc624435d722b10bc2e66
Louise <louise.skinnari@cern.ch>
2014-03-10 15:21:44 +0100

    add updated config file for jets

--- SLHCUpgradeSimulations/L1TrackTriggerObjects/test/runHIJets.py
+++ SLHCUpgradeSimulations/L1TrackTriggerObjects/test/runHIJets.py
@@ -84,13 +84,55 @@ process.L1JetsFromHIHLTJets = cms.EDProducer("L1JetsFromHIHLTJets",
 process.pL1Jets = cms.Path( process.L1JetsFromHIHLTJets )
 
 
-# --- Produce L1TkJets
-process.L1TkJets = cms.EDProducer("L1TkJetProducer",
-        L1CentralJetInputTag = cms.InputTag("L1JetsFromHIHLTJets"),     
-        L1TrackInputTag = cms.InputTag("L1Tracks","Level1TkTracks"),
-)
-process.pJets = cms.Path( process.L1TkJets )
+############################################################
+# track trigger
+############################################################
 
+process.load('Configuration.StandardSequences.L1TrackTrigger_cff')
+process.load('Geometry.TrackerGeometryBuilder.StackedTrackerGeometry_cfi')
+process.load("SLHCUpgradeSimulations.L1TrackTrigger.L1TTrack_cfi")
+process.L1Tracks.geometry = cms.untracked.string('BE5D')
+
+# Remake stubs (running with zMatching=False)
+process.L1TrackTrigger_step = cms.Path(process.L1TrackTrigger)
+
+# Make L1 tracks
+process.BeamSpotFromSim = cms.EDProducer("BeamSpotFromSimProducer")
+process.TT_step = cms.Path(process.BeamSpotFromSim*process.L1Tracks)
+
+
+############################################################
+# run the L1TkJetProducer 
+############################################################
+
+process.L1TkJets = cms.EDProducer("L1TkJetProducer",
+                                  #L1CentralJetInputTag = cms.InputTag("l1extraParticles","Central"),
+                                  L1CentralJetInputTag = cms.InputTag("L1JetsFromHIHLTJets",""),     
+                                  L1TrackInputTag = cms.InputTag("L1Tracks","Level1TkTracks"),
+                                  TRK_ZMAX = cms.double(25.),     # max track z0 [cm]
+                                  TRK_CHI2MAX = cms.double(100.), # max track chi2
+                                  TRK_PTMIN = cms.double(2.0),    # minimum track pt [GeV]
+                                  TRK_ETAMAX = cms.double(2.5),   # maximum track eta
+                                  TRK_NSTUBMIN = cms.int32(4),    # minimum number of stubs on track
+                                  TRK_NSTUBPSMIN = cms.int32(0),  # DOESN'T WORK, KEEP AT ZERO !!! minimum number of stubs in PS modules on track        
+)
+process.pJets = cms.Path(process.L1TkJets)
+
+
+############################################################
+# run the L1TkHTMissProducer 
+############################################################
+
+process.L1TkHTMiss = cms.EDProducer("L1TkHTMissProducer",
+                                    L1TkJetInputTag = cms.InputTag("L1TkJets","Central"),
+                                    JET_PTMIN = cms.double(20.0),          # minimum pt of jets for HT [GeV]
+                                    JET_HLTETA = cms.bool(True),           # temporary hack, for HLT jets remove jets from bad eta regions
+                                    DoVtxConstrain = cms.bool(True),       # turn on/off applying any vertex constraint
+                                    DeltaZ = cms.double(1.0),              # require jets to have |z_jet - z_ref| < DeltaZ [cm]
+                                    PrimaryVtxConstrain = cms.bool(False), # use primary vertex instead of leading jet as reference z position
+                                    L1VertexInputTag = cms.InputTag("L1TrackPrimaryVertex")
+)
+process.pHTM = cms.Path(process.L1TkHTMiss)
 
 
 
@@ -107,6 +149,7 @@ process.Out.outputCommands.append('keep *_l1extraParticles_*_*')
 process.Out.outputCommands.append('keep *_iterativeConePu5CaloJets_*_*')
 process.Out.outputCommands.append('keep *_L1JetsFromHIHLTJets_*_*')
 process.Out.outputCommands.append('keep *_L1TkJets_*_*')
+process.Out.outputCommands.append('keep *_L1TkHTMiss_*_*')
 
 
 process.FEVToutput_step = cms.EndPath(process.Out)
