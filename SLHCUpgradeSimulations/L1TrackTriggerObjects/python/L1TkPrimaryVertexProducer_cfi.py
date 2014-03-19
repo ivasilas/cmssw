76c77639a14fd244703fa60308d6e5c14f355922
Louise <louise.skinnari@cern.ch>
2014-03-19 10:42:31 +0100

    add v17 to my repo

---
+++ SLHCUpgradeSimulations/L1TrackTriggerObjects/python/L1TkPrimaryVertexProducer_cfi.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+L1TrackPrimaryVertex = cms.EDProducer('L1TkFastVertexProducer',
+     L1TrackInputTag = cms.InputTag("L1Tracks","Level1TkTracks"),
+     ZMAX = cms.double ( 25. ) ,        # in cm
+     CHI2MAX = cms.double( 100. ),
+     PTMINTRA = cms.double( 2.),        # PTMIN of L1Tracks, in GeV
+     nStubsmin = cms.int32( 4 ) ,       # minimum number of stubs
+     nStubsPSmin = cms.int32( 3 )       # minimum number of stubs in PS modules 
+)
